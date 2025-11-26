ca0adc0e21dadee35696cd8e49116f579116b608
UMIDRR <umidrzayev52@gmail.com>
2025-11-26 23:45:00 +0400

    Fix case sensitivity for Hero.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import Navbar from "./components/Navbar.jsx";
-import Hero from "./components/hero.jsx";
+import Hero from "./components/Hero.jsx";
 import About from "./components/About.jsx";
 import Projects from "./components/Projects.jsx";
 import Footer from "./components/Footer.jsx";
+
 
 function App() {
   return (
